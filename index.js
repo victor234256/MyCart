2348c5f48fdf1e5aa001d158ec7a8441c213a7b5
macbook <user@macbooks-MacBook-Pro.local>
2023-01-24 06:06:26 +0100

    toggle nav-icons

--- index.js
+++ index.js
@@ -3,4 +3,41 @@ let searchform = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
 searchform.classList.toggle('active');
+    shopping_cart.classList.remove('active');
+    login_form.classList.remove('active');
+    nav_icon.classList.remove('active');
+}
+
+let shopping_cart = document.querySelector('.shopping-cart');
+
+
+document.querySelector('#cart-btn').onclick = () => {
+shopping_cart.classList.toggle('active');
+    searchform.classList.remove('active');
+    login_form.classList.remove('active');
+    nav_icon.classList.remove('active');
+}
+let login_form = document.querySelector('.login-form');
+
+
+document.querySelector('#login-btn').onclick = () => {
+login_form.classList.toggle('active');
+    searchform.classList.remove('active');
+    shopping_cart.classList.remove('active');
+    nav_icon.classList.remove('active');
+}
+let nav_icon = document.querySelector('.navbar');
+
+
+document.querySelector('#menu-btn').onclick = () => {
+nav_icon.classList.toggle('active');
+    searchform.classList.remove('active');
+    shopping_cart.classList.remove('active');
+    login_form.classList.remove('active');
+}
+window.onscroll = () => {
+    searchform.classList.remove('active');
+    shopping_cart.classList.remove('active');
+    login_form.classList.remove('active');
+    nav_icon.classList.remove('active');
 }
