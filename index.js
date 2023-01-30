0b0403422bbc11098f291d1d0c8ddd6751d3ee2e
macbook <user@macbooks-MacBook-Pro.local>
2023-01-31 00:52:53 +0100

    body added

--- index.js
+++ index.js
@@ -64,3 +64,26 @@ var swiper = new Swiper(".product-slider", {
       },
     },
   });
+
+  var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+       
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1020: {
+        slidesPerView: 3,
+       
+      },
+    },
+  });
